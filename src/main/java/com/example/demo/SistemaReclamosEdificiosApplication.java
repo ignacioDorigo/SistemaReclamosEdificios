ccc58c61a9270f5a8104a33c42087690937d75a7
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2023-10-12 08:43:49 -0300

    primer push

---
+++ src/main/java/com/example/demo/SistemaReclamosEdificiosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReclamosEdificiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReclamosEdificiosApplication.class, args);
+	}
+
+}
